99653dd46e9d07c30e80dc0fc49ad46c16940faf
Leon Carbonell <leoncarbonell@gmail.com>
2025-03-30 14:57:59 +1030

    using motion

---
+++ app/_components/hero-section.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { motion } from 'motion/react'
+
+export default function HeroSection() {
+  return (
+    <>
+      <div className="relative h-[42rem] w-full overflow-hidden bg-gradient-to-br from-yellow-600 to-amber-600">
+        Cats
+      </div>
+    </>
+  )
+}
